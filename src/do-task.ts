e5e3f146971694cb785d2e6013b6158405a74db7
kemokemo <t2wonderland@gmail.com>
2023-03-12 11:36:02 +0900

    feat: cancel task with child process

--- src/do-task.ts
+++ src/do-task.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import cp = require('child_process');
 
 const sleep = (time: number) => {
 	return new Promise((resolve) => {
@@ -9,8 +10,14 @@ const sleep = (time: number) => {
 };
 
 export function doTask() {
+	const controller = new AbortController();
+	const { signal } = controller;
+	const child = cp.exec('sleep 10000', { signal }, (error) => {
+		console.log(`${error}`);
+	});
+
 	vscode.window.withProgress({
-		title: 'Please wait...',
+		title: 'Sample Task',
 		location: vscode.ProgressLocation.Notification,
 		cancellable: true
 	},
@@ -18,14 +25,19 @@ export function doTask() {
 			const seconds = 10;
 			for (let i = 0; i < seconds; i++) {
 				if(token.isCancellationRequested){
+					vscode.window.showWarningMessage("Cancelling task. Please wait...");
+					controller.abort();
+					child.kill();
+					await sleep(1000); // Emulation of time to cancel
+
 					vscode.window.showWarningMessage("Sample Task: User cancelled.");
 					return;
 				}
-				progress.report({ increment: seconds });
+				// you can catch messages from script via some env variables etc..
+				progress.report({ message: 'Running...', increment: seconds });
 				await sleep(1000);
 			}
 			vscode.window.showInformationMessage("Sample Task: Task has completed.");
 		}
 	);
-
 }
